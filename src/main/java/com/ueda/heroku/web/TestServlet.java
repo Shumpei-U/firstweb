20785caead0eea70632c58367e2e4559de901f2e
Sueda <west5lakers23@gmail.com>
2020-11-25 21:58:11 +0900

    servletを変更

--- src/main/java/com/ueda/heroku/web/TestServlet.java
+++ src/main/java/com/ueda/heroku/web/TestServlet.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlet;
+ package com.ueda.heroku.web;
 
-import JavaBeans.testBeans;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -32,60 +31,11 @@ public class TestServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
-    @Override
-     protected void doGet(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-         this.doPost(req,res);
-     }
-    
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-        res.setContentType("text/html;charset=UTF-8");
-        
-        String message;
-        String name;
-        Integer counter;
-        
-        RequestDispatcher rd;
-        HttpSession session = req.getSession(false);
-        
-        //セッション獲得
-        if(session == null){
-            session = req.getSession(true);
-            message = "This is first access";
-            counter = new Integer(1);
-        }else {
-            counter = (Integer)session.getAttribute("counter");
-            if(counter == 2){
-                message = "This is second times access";
-            }else if(counter == 3){
-                message = "This is third times access";
-            }else{
-                message = "This is " + counter + "th times access";
-                }
-        }
-        
-        //カウンターセット
-        counter++;
-        session.setAttribute("counter",counter );
-        session.setAttribute("message",message );
-        //Formデータ獲得
-        name = req.getParameter("name");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.getWriter().print("Hello Shunji!");
+    }     
 
-        //Formデータをセッションにセット
-        testBeans tb = new testBeans();
-        tb.setName(name);
-        tb.setMessage(message);
-        
-        session.setAttribute("testbeans",tb);
-        
-        //フォワード
-        rd = req.getRequestDispatcher("/JSP/testJsp.jsp");
-        rd.forward(req, res);
-        
-        }
        
 
 
